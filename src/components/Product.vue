3aaa1747003c3a923a66efbfe4d9cc4061035895
gaianacheli <gaianacheli04@gmail.com>
2021-11-12 11:53:56 +0100

    creato componente non ancora funzionante

---
+++ src/components/Product.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="card">
+      <img :src="image" alt="">
+      <h3>{{title}}</h3>
+  </div>
+    
+</template>
+
+
+<script>
+export default {
+    props:{
+        image: String,
+        title: String
+    }
+}
+</script>
